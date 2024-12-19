62a4a0427270436f79aa92204de95a55a451f248
Adam Cmiel <acmiel@redhat.com>
2024-12-20 00:45:17 +0100

    Day 19: part 2
    Signed-off-by: Adam Cmiel <acmiel@redhat.com>

--- 19/solve.py
+++ 19/solve.py
@@ -1,16 +1,16 @@
 import functools
 
 
-def count_possible(designs: list[str], towels: list[str]) -> int:
+def count_ways_to_make(designs: list[str], towels: list[str]) -> list[int]:
     @functools.cache
-    def is_possible(design: str) -> bool:
+    def ways_to_make(design: str) -> int:
         if not design:
-            return True
-        return any(
-            design.startswith(towel) and is_possible(design[len(towel) :]) for towel in towels
+            return 1
+        return sum(
+            ways_to_make(design[len(towel) :]) for towel in towels if design.startswith(towel)
         )
 
-    return sum(map(is_possible, designs))
+    return list(map(ways_to_make, designs))
 
 
 def parse_input(inp: str) -> tuple[list[str], list[str]]:
@@ -18,15 +18,13 @@ def parse_input(inp: str) -> tuple[list[str], list[str]]:
     return list(map(str.strip, towels.split(","))), designs
 
 
-def part1(towels: list[str], designs: list[str]) -> int:
-    return count_possible(designs, towels)
-
-
 def main() -> None:
     with open("input.txt") as f:
         towels, designs = parse_input(f.read())
 
-    print(part1(towels, designs))
+    ways_to_make = count_ways_to_make(designs, towels)
+    print(sum(n > 0 for n in ways_to_make))
+    print(sum(ways_to_make))
 
 
 if __name__ == "__main__":
