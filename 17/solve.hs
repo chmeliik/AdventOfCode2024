43bfef834d658bdb35a399e8f6900d9e94067b5c
Adam Cmiel <acmiel@redhat.com>
2024-12-19 22:52:19 +0100

    Day 17: part 2
    Signed-off-by: Adam Cmiel <acmiel@redhat.com>

--- 17/solve.hs
+++ 17/solve.hs
@@ -1,10 +1,10 @@
 module Main (main) where
 
 import Control.Monad.State (State, evalState, gets, modify)
-import Data.Array.IArray (Array, bounds, listArray, (!))
-import Data.Bits (shiftR, xor)
-import Data.List (foldl', intercalate)
-import Data.Maybe (catMaybes)
+import Data.Array.IArray (Array, bounds, elems, listArray, (!))
+import Data.Bits (shiftL, shiftR, xor)
+import Data.List (find, foldl', intercalate)
+import Data.Maybe (catMaybes, fromJust)
 
 data ProgramState = ProgramState
   { pInstructionPointer :: Int,
@@ -94,8 +94,8 @@ evalInstr instr n = case instr of
 
 type Program = Array Int Int
 
-evalAll :: Program -> RunProg [Maybe Int]
-evalAll program = do
+runProg :: Program -> RunProg [Maybe Int]
+runProg program = do
   pointer <- gets pInstructionPointer
   let (_, end) = bounds program
   if pointer >= end
@@ -105,11 +105,58 @@ evalAll program = do
       let instr = toEnum (program ! pointer)
       let operand = program ! (pointer + 1)
       v <- evalInstr instr operand
-      vs <- evalAll program
+      vs <- runProg program
       pure $ v : vs
 
 evalProgram :: ProgramState -> Program -> [Maybe Int]
-evalProgram p instrs = evalState (evalAll instrs) p
+evalProgram p instrs = evalState (runProg instrs) p
+
+-- My program is:
+--
+-- BST 4
+-- BXL 2
+-- CDV 5
+-- BXC 5
+-- ADV 3
+-- BXL 7
+-- OUT 5
+-- JNZ 0
+--
+-- A couple interesting properties of this program:
+--
+-- * it's a simple do ... while loop, the jump is at the end and jumps to the beginning
+-- * the value of register A rightshifts by 3 in every iteration
+--   (=> the program definitely terminates)
+-- * there is one OUT instruction => each iteration outputs one element
+-- * the output of each iteration depends on *and only on* the value of A
+--   * BST 4 => B = A % 8
+--   * BXL 2 => B = B ^ 2
+--   * CDV 5 => C = A >> B
+--   * at this point, B and C have both been initialized based on nothing but A
+--
+-- To find the initial A such that the program returns the desired output:
+--
+-- * Search the output reversed, we know A is 0 at the end
+-- * For each number in the reversed output:
+--   * leftshift the current A by 3 to invert the rightshift
+--   * the rightshift also dropped the last three bits, so you actually need to try
+--     all 8 different possible values that would rightshift down to the current A
+--   * find the first of the 8 possible previous As that returns the desired number
+--   * make that the current A
+-- * When you reach the end of the reversed output, the current A is the initial A
+
+findInitialA :: Program -> [Int] -> Int
+findInitialA prog forOutput = foldl' previousA 0 (reverse forOutput)
+  where
+    previousA :: Int -> Int -> Int
+    previousA currentA out = fromJust $ find returnsOut possiblePrevAs
+      where
+        leastPrevA = currentA `shiftL` 3
+        possiblePrevAs = [leastPrevA .. leastPrevA + 7]
+
+        returnsOut :: Int -> Bool
+        returnsOut a =
+          (== out) $ head $ catMaybes $ evalProgram (newState {pRegisterA = a}) prog
 
 parseInput :: String -> (ProgramState, Program)
 parseInput input = (programState, listArray (0, length program - 1) program)
@@ -134,7 +181,11 @@ parseInput input = (programState, listArray (0, length program - 1) program)
 part1 :: (ProgramState, Program) -> String
 part1 = intercalate "," . map show . catMaybes . uncurry evalProgram
 
+part2 :: (ProgramState, Program) -> Int
+part2 (_, prog) = findInitialA prog (elems prog)
+
 main :: IO ()
 main = do
   input <- parseInput <$> readFile "input.txt"
   putStrLn $ part1 input
+  print $ part2 input
